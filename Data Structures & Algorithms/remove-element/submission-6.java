a02df8c1f98679914ea4c41ab939f91c26003bac
David-Gabriel Pop <04davidpop@gmail.com>
2026-09-16 17:10:27 +0300

    Add: remove-element - submission-6

---
+++ Data Structures & Algorithms/remove-element/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int right = nums.length - 1, left = 0;
+        while (right >= 0 && left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right--];
+            }
+            else 
+                left++;
+        }
+
+        return right + 1;
+    }
+}
