5a0f0d635c41e876fddf8b1757a0c1aa5f2ad5e9
David-Gabriel Pop <04davidpop@gmail.com>
2026-09-16 22:17:11 +0300

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.java
@@ -0,0 +1,30 @@
+// boolean approach
+
+class MyHashSet {
+
+    boolean[] set;
+    public MyHashSet() {
+        set = new boolean[100000000];
+    }
+    
+    public void add(int key) {
+        if (!set[key])
+            set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
